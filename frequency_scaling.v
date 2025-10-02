ad71661172c5c346a70b352543ab93b3756e4963
nikitarewad <nikita441102@gmail.com>
2025-10-03 03:37:51 +0530

    Added Maze Solver Bot files

---
+++ frequency_scaling.v
@@ -0,0 +1,20 @@
+
+module frequency_scaling (
+    input clk_50M,
+    output reg clk_3125KHz
+);
+
+initial begin
+    clk_3125KHz = 0;
+end
+//////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE //////////////////
+reg [3:0] count = 8;
+reg [3:0] counter = 0;
+
+always @(posedge clk_50M) begin
+    counter <= counter + 1'b1;
+	 clk_3125KHz <= (counter < count);
+end
+//////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE //////////////////
+
+endmodule
---
+++ pwm_generator.v
@@ -0,0 +1,22 @@
+
+module pwm_generator(
+    input clk_3125KHz,
+    input [3:0] duty_cycle,
+    output reg clk_195KHz, pwm_signal
+);
+
+initial begin
+    clk_195KHz = 0; pwm_signal = 1;
+end
+//////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE //////////////////
+reg [3:0] count = 8;
+reg [3:0] counter = 0;
+
+always @(posedge clk_3125KHz) begin
+    counter <= counter + 1'b1;
+    pwm_signal <= (counter < duty_cycle);
+	 clk_195KHz <= (counter < count);
+end
+//////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE //////////////////
+
+endmodule
